db7b365887c69e2236a146f8065e2bb1ab1e74ba
8msbz8 <8msbz8@gmail.com>
2018-10-18 19:18:21 +0300

    Add project files.

---
+++ CompProj/FlatFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompProj {
+    public class FlatFile {
+        public string Data { get; set; }
+
+        public FlatFile(string pathToFile) {
+
+        }
+    }
+}
